2f139226431ef33cc1653fe2b94fe292e6b1b3b8
gao830 <billgao830@gmail.com>
2017-09-07 23:35:21 +0000

    Style index page, show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  
+  
+  # def description=(val)
+  #   write_attribute(:description, val.gsub("\u00A0", " "))
+  
+  # end
+  
+  
+  
+  
 end 
